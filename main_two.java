1ecea59e5a7fed356c4284c33a5bc6db1bfa345a
Maximilian Merten <maximilian.merten@ewerk.com>
2023-11-13 13:49:15 +0100

    changed Forward / Backslash, typos, changed height to days

--- main_two.java
+++ main_two.java
@@ -6,41 +6,41 @@ public class main_two {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Geben Sie die Tag (0-24):");
-        int height = scanner.nextInt();
+        int days = scanner.nextInt();
 
-        if (height >= 0 && height <= 24) {
-            drawChristmasTree(height);
+        if (days >= 0 && days <= 24) {
+            drawChristmasTree(days);
         } else {
             System.out.println("Ungültige Eingabe. Der Tag muss zwischen 0 und 24 liegen.");
         }
     }
 
-    private static void drawChristmasTree(int height) {
-        //kerzen Oben
+    private static void drawChristmasTree(int days) {
+        //Kerzen Oben
         for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < height; j++) {
+            for (int j = 0; j < days; j++) {
                 System.out.print(" ");
             }
             System.out.println("ii");
         }
 
-        for (int i = 0; i < height; i++) {
+        for (int i = 0; i < days; i++) {
             // Leerzeichen für die Ausrichtung
-            for (int j = 0; j < height - i - 1; j++) {
+            for (int j = 0; j < days - i - 1; j++) {
                 System.out.print(" ");
             }
-            // Backslash für die linke Umrandung
+            // Forwardslash für die linke Umrandung
             System.out.print("i/");
             // Sterne für den Baum
             for (int k = 0; k < 2 * i + 1; k++) {
                 System.out.print("*");
             }
-            // Forwardslash für die rechte Umrandung
+            // Backslash für die rechte Umrandung
             System.out.println("\\i");
         }
 
         for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < height * 2; j++) {
+            for (int j = 0; j < days * 2 + 1; j++) {
                 System.out.print('-');
             }
             System.out.println("-");
@@ -48,7 +48,7 @@ public class main_two {
 
         // Stamm des Baums
         for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < height; j++) {
+            for (int j = 0; j < days; j++) {
                 System.out.print(" ");
             }
             System.out.println("||");
